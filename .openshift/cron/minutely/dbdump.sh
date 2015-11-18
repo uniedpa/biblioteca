7cbc72f54ba8159ba537e4eda83fa9334cdaa842
omar vasquez lima <omar@vasquezlima.com>
2015-11-18 13:10:13 -0500

    Tareas Cron para respaldo de base de datos

---
+++ .openshift/cron/minutely/dbdump.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ `date +%H:%M` == "13:30" ]
+then
+	mysqldump -u $OPENSHIFT_MYSQL_DB_USERNAME  -p$OPENSHIFT_MYSQL_DB_PASSWORD biblioteca -h $OPENSHIFT_MYSQL_DB_HOST | gzip -> $OPENSHIFT_DATA_DIR/Dropbox/sistema/base-de-datos/mysqldump-servidor-$(date +%Y-%m-%d-%H.%M.%S).gz
+fi
