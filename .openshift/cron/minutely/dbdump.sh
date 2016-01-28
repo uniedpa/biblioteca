ccd19d19710455b17d3fea0e0a1a82c0593767ca
Omar Vasquez Lima <omar@vasquezlima.com>
2016-01-28 14:36:16 -0500

    Usando guion bash Dropbox-Uploader https://github.com/andreafabrizi/Dropbox-Uploader donde se modificado la ubicacion del archivo de configuracion

--- .openshift/cron/minutely/dbdump.sh
+++ .openshift/cron/minutely/dbdump.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#if [ `date +%H:%M` == "23:59" ]
-#then
-#	mysqldump -u $OPENSHIFT_MYSQL_DB_USERNAME  -p$OPENSHIFT_MYSQL_DB_PASSWORD biblioteca -h $OPENSHIFT_MYSQL_DB_HOST | gzip -> $OPENSHIFT_DATA_DIR/Dropbox/sistema/base-de-datos/mysqldump-servidor-$(date +%Y-%m-%d-%H.%M.%S).gz
-#fi
+if [ `date +%H:%M` == "23:59" ]
+then
+	timestamp=$(date +%Y-%m-%d-%H.%M.%S)
+	mysqldump -u $OPENSHIFT_MYSQL_DB_USERNAME  -p$OPENSHIFT_MYSQL_DB_PASSWORD biblioteca -h $OPENSHIFT_MYSQL_DB_HOST | gzip -> $OPENSHIFT_DATA_DIR/Dropbox/sistema/base-de-datos/mysqldump-servidor-$timestamp.gz
+	$OPENSHIFT_DATA_DIR/Dropbox-Uploader/dropbox_uploader.sh upload $OPENSHIFT_DATA_DIR/Dropbox/sistema/base-de-datos/mysqldump-servidor-$timestamp.gz
+fi
